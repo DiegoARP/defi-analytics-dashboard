f48970437b1ed4096f9a2c6dbbc1b9841432cb99
DiegoARP <diegoarzp@gmail.com>
2024-11-12 19:14:43 -0600

    updated next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   experimental: {
-    optimizeCss: true
+    optimizeCss: true,
   }
 }
 
